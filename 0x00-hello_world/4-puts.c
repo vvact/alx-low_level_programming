4563e4637dc37d57b506bab29afee16aa7efb4ce
Victor Bunyali Midega <vbm7752@gmail.com>
2023-02-16 01:50:24 -0800

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@ int main(void)
  * Return: Always 0 (Success)
  */
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");/* inline comment */
 		return 0;
 }
