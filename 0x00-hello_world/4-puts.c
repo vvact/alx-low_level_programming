0891fe9d59a21e824de0385b6576c46075832d54
Victor Bunyali Midega <vbm7752@gmail.com>
2023-02-16 04:13:03 -0800

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - prints "Programming is like building a multilingual puzzle."
- * Return: Always 0
- */
-
+* main - prints "Programming is like building"
+* Return: Always 0 (success)
+*/
 int main(void)
-
 {
-        puts("\"Programming is like building a multilingual puzzle");	
-        
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
