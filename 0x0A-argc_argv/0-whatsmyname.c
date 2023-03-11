1e0cce35ca725a58912ee300cd25003141453c76
Victor Bunyali Midega <vbm7752@gmail.com>
2023-03-11 03:04:58 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - prints the numnret
+* @argc: number of arguments
+* @argv: yougsjhyug
+* Return: Always 0 (Success)
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
