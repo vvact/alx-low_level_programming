1b54a000c0dc8dabd25f9a96c22f0594ccade4b2
Victor Midega <vbm7752@gmail.com>
2023-06-09 14:12:20 -0700

    mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: the array
+ * Return: 0 sucesss
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
